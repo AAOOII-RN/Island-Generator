814fc3e133760bf44ed1185ce03791ad99a2dc5d
AAOOII-RN <aaooii.rn@gmail.com>
2025-07-03 16:30:14 +0800

    Added travelling

--- main.lua
+++ main.lua
@@ -2,16 +2,6 @@
 Todo:
 Optimize the generator
 ]]
-
-function love.load()
-    randoms = 1
-    offset = {}
-    amp = {}
-    do_once = false
-
-    t = 0
-end
-
 function hillNoise(x, y, n)
     if not do_once then
         for j = 1, randoms do
@@ -28,10 +18,53 @@ function hillNoise(x, y, n)
     return math.sin((x*amp[n][1]+offset[n][1] + y*amp[n][2]+offset[n][2])*5^0.5/2) * math.sin((x*amp[n][3]+offset[n][3] - y*amp[n][4]+offset[n][4])*3^0.5) * math.sin((x*amp[n][5]+offset[n][5] + y*amp[n][6]+offset[n][6])*2^0.5)
 end
 
+function love.load()
+    randoms = 2
+    offset = {}
+    amp = {}
+    do_once = false
+
+    screen = {}
+    screen.quality = 6
+    screen.cellSize = 7
+
+    screen.range = 0.05
+
+    tx = 0
+    ty = 0
+end
+
+
 function love.update(dt)
-    t = t + 1 * dt
+    local kd = love.keyboard.isDown
+    if kd("w") then
+        ty = ty + 100 * dt
+    elseif kd("s") then
+        ty = ty - 100 * dt
+    end
+
+    if kd("a") then
+        tx = tx + 100 * dt
+    elseif kd("d") then
+        tx = tx - 100 * dt
+    end
+
+    for y = 1, 2^screen.quality do
+        screen[y] = {}
+        for x = 1, 2^screen.quality do
+            screen[y][x] = hillNoise((x-tx)*screen.range, (y-ty)*screen.range, 1) * hillNoise((x-tx)*screen.range, (y-ty)*screen.range, 2)
+        end
+    end
 end
 
 function love.draw()
-    
+    love.graphics.setBackgroundColor(0.9,0.9,0.9)
+    for y, row in ipairs(screen) do
+        for x, tile in ipairs(row) do
+            love.graphics.setColor(0.1,0.1,0.1, math.max(tile, 0))
+            love.graphics.rectangle("fill", x * screen.cellSize, y * screen.cellSize, screen.cellSize, screen.cellSize)
+        end
+    end
+
+    love.graphics.setColor(1,1,1,1)
 end
