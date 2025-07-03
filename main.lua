a8b2b3dfa3084aa3c6f8c39853729e94e22f5d87
AAOOII-RN <aaooii.rn@gmail.com>
2025-07-03 22:15:53 +0800

    Finished

--- main.lua
+++ main.lua
@@ -1,8 +1,5 @@
---[[
-Todo:
-Optimize the generator
-]]
 function hillNoise(x, y, n)
+    math.randomseed(os.clock())
     if not do_once then
         for j = 1, randoms do
             offset[j] = {}
@@ -15,20 +12,35 @@ function hillNoise(x, y, n)
         do_once = true
     end
 
-    return math.sin((x*amp[n][1]+offset[n][1] + y*amp[n][2]+offset[n][2])*5^0.5/2) * math.sin((x*amp[n][3]+offset[n][3] - y*amp[n][4]+offset[n][4])*3^0.5) * math.sin((x*amp[n][5]+offset[n][5] + y*amp[n][6]+offset[n][6])*2^0.5)
+    xv = {}
+    yv = {}
+    for i = 1, 6 do
+        xv[math.ceil(i/2)] = x*amp[n][math.ceil(i/2)*2-1]+offset[n][math.ceil(i/2)*2-1]
+        yv[math.ceil(i/2)] = y*amp[n][math.ceil(i/2)*2]+offset[n][math.ceil(i/2)*2]
+    end
+
+    return math.sin(xv[1] + yv[1] * 5^0.5/2) * math.sin(xv[2] - yv[2] * 3^0.5) * math.sin(xv[3] + yv[3] * 2^0.5)
 end
 
 function love.load()
+    ww, wh = love.window.getMode()
     randoms = 2
     offset = {}
     amp = {}
     do_once = false
 
     screen = {}
-    screen.quality = 6
-    screen.cellSize = 7
+    screen.quality = 256
+    screen.cellSize = 3
 
-    screen.range = 0.05
+    screen.range = 0.01
+    screen.colors = {
+        {0.243, 0.729, 0.286},
+        {1, 0.89, 0.89},
+        {0, 0.761, 1},
+        {0, 0.549, 1},
+        {0, 0.416, 0.831}
+    }
 
     tx = 0
     ty = 0
@@ -36,35 +48,77 @@ end
 
 
 function love.update(dt)
+    screen.range = math.max(screen.range, 0.005)
     local kd = love.keyboard.isDown
+
+    -- TRAVEL
     if kd("w") then
-        ty = ty + 100 * dt
+        ty = ty + 10 * dt
     elseif kd("s") then
-        ty = ty - 100 * dt
+        ty = ty - 10 * dt
     end
 
     if kd("a") then
-        tx = tx + 100 * dt
+        tx = tx + 10 * dt
     elseif kd("d") then
-        tx = tx - 100 * dt
+        tx = tx - 10 * dt
     end
 
-    for y = 1, 2^screen.quality do
+    -- ZOOM
+    if kd("q") then -- Zoom out
+        screen.range = screen.range + 0.1 * dt
+    elseif kd("e") then -- Zoom in
+        screen.range = screen.range - 0.1 * dt
+    end
+
+    for y = 1, screen.quality do
         screen[y] = {}
-        for x = 1, 2^screen.quality do
-            screen[y][x] = hillNoise((x-tx)*screen.range, (y-ty)*screen.range, 1) * hillNoise((x-tx)*screen.range, (y-ty)*screen.range, 2)
+        for x = 1, screen.quality do
+            local a = hillNoise(
+                tx-screen.range*(x-screen.quality/2), -- Idk how either of these works...
+                ty-screen.range*(y-screen.quality/2),
+                1
+            )
+            local b = hillNoise(
+                tx-screen.range*(x-screen.quality/2),
+                ty-screen.range*(y-screen.quality/2),
+                2
+            )
+
+            noise =  a * b
+            screen[y][x] = noise
         end
     end
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(0.9,0.9,0.9)
+    love.graphics.setBackgroundColor(0.1,0.1,0.1)
     for y, row in ipairs(screen) do
         for x, tile in ipairs(row) do
-            love.graphics.setColor(0.1,0.1,0.1, math.max(tile, 0))
+            if tile >= 0.25 then
+                love.graphics.setColor(screen.colors[1])
+            elseif tile >= 0.2 then
+                love.graphics.setColor(screen.colors[2])
+            elseif tile >= 0.125 then
+                love.graphics.setColor(screen.colors[3])
+            elseif tile >= 0.025 then
+                love.graphics.setColor(screen.colors[4])
+            else
+                love.graphics.setColor(screen.colors[5])
+            end
+
             love.graphics.rectangle("fill", x * screen.cellSize, y * screen.cellSize, screen.cellSize, screen.cellSize)
         end
     end
 
-    love.graphics.setColor(1,1,1,1)
+    love.graphics.setColor(0.9, 0.9, 0.9)
+    -- Title, Credits, Pixels, Coordinates, Zoom, Quote
+    love.graphics.print("Island Generator", 2*ww/3, wh/10)
+    love.graphics.print("Made By AAOOII-RN and with Love", 2*ww/3, 2*wh/10)
+    love.graphics.print("# of Tiles & Tile Size: " .. screen.quality^2 .. ", " .. screen.cellSize, 2*ww/3, 3*wh/10)
+    love.graphics.print("Coordinates: " .. math.floor(tx) .. ", " .. math.floor(ty), 2*ww/3, 4*wh/10)
+    love.graphics.print("Zoom: " .. math.floor(screen.range*100), 2*ww/3, 5*wh/10)
+    love.graphics.print("FPS: " .. love.timer.getFPS(), 2*ww/3, 6*wh/10)
+    love.graphics.print("WASD to move, Q and E to zoom", 2*ww/3, 7*wh/10)
+    love.graphics.print('"You may encounter an obvious pattern.\nRestart the program if you hate it"', 2*ww/3, 8*wh/10)
 end
